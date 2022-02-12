42dab571c5bffafa7f9b1e8965de9483ca6b4352
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-02-12 19:48:11 +0000

    new todo form from class to function

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,44 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './NewTodoForm.css'
 
 
-class NewTodoForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { name: '' }
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+function NewTodoForm(props) {
+    const [name, setName] = useState('');
+    const handleChange = (ev) => {
+        setName(ev.target.value)
     }
-    handleChange(ev) {
-        this.setState({
-            [ev.target.name]: ev.target.value
-        })
-    }
-    handleSubmit(ev) {
+    const handleSubmit = (ev) => {
         ev.preventDefault();
-        if (this.state.name.length > 0) {
-            let newTodo = { ...this.state, id: uuidv4(), completed: false };
-            this.props.addTodo(newTodo);
-            this.setState({ name: '' })
+        if (name.length > 0) {
+            let newTodo = { name, id: uuidv4(), completed: false };
+            props.addTodo(newTodo);
+            setName('');
         }
     }
-    render() {
-        return (
-            <form className='NewTodoForm' onSubmit={this.handleSubmit}>
-                <label htmlFor='name'>New Todo</label>
-                <input
-                    maxLength="20"
-                    type='text'
-                    name='name'
-                    id='name'
-                    value={this.state.name}
-                    onChange={this.handleChange}
-                ></input>
-                <button>Add</button>
-            </form>
-        )
-    }
+    return (
+        <form className='NewTodoForm' onSubmit={handleSubmit}>
+            <label htmlFor='name'>New Todo</label>
+            <input
+                maxLength="20"
+                type='text'
+                name='name'
+                id='name'
+                value={name}
+                onChange={handleChange}
+            ></input>
+            <button>Add</button>
+        </form>
+    )
 }
 
 export default NewTodoForm;
